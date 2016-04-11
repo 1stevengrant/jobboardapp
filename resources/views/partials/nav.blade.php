3b671a93a2db1d53a14ea4c7c129c9d630ea1443
Steven Grant <steven@ghijk.co.uk>
2016-04-11 22:13:16 +0100

    Added my profile to the nav

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -34,10 +34,11 @@
                     @else
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->name }} <span class="badge badge-info">{{$user->id }}</span><span class="caret"></span>
+                                {{ Auth::user()->name }} <span class="caret"></span>
                             </a>
 
                             <ul class="dropdown-menu" role="menu">
+                                <li><a href="{{ url('/profile') }}"><i class="fa fa-btn fa-user"></i>My Profile</a></li>
                                 <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
                             </ul>
                         </li>
