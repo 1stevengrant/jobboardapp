b916fb1709a4e843b960d410cc16f3dbed7f7453
Steven Grant <steven@ghijk.co.uk>
2016-04-12 01:06:42 +0100

    new migrations for profiles tables

---
+++ database/migrations/2016_04_11_234813_create_profiles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('org_type');
+            $table->text('org_url');
+            $table->text('org_twitter');
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
+        });
+        Schema::table('profiles', function($table) {
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('profiles');
+    }
+}
