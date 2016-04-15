cc4c6ddfc5ee48de31ea0f54133c744d0aad7a23
Steven Grant <steven@ghijk.co.uk>
2016-04-15 23:13:36 +0100

    added a simple 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('title', 'Not Found')
+
+@section('content')
+
+    <div class="row">
+        <div class="content-md-12">
+            <div class="jumbotron">
+                <h1>Not Found</h1>
+            </div>
+        </div>
+    </div>
+
+@stop
+
+@section('scripts')
+
+@stop
