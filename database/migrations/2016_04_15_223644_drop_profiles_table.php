ba8f82d47a5fb3a07fbdd557f7f8118124a2de22
Steven Grant <steven@ghijk.co.uk>
2016-04-15 23:37:32 +0100

    dropped the profiles table in migration

---
+++ database/migrations/2016_04_15_223644_drop_profiles_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DropProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('profiles', function (Blueprint $table) {
+            $table->drop();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('profiles', function (Blueprint $table) {
+            //
+        });
+    }
+}
