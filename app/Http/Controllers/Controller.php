ddfe051e72993d0530f3195e40fdfc683aa5bd9f
Steven Grant <steven@ghijk.co.uk>
2016-04-15 23:31:59 +0100

    Added some common variables to the master controller to share in views

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Auth;
+use Carbon\Carbon;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -16,7 +17,17 @@ class Controller extends BaseController
 
     public function __construct()
     {
+
+        $this->currentUser = auth()->user();
+        view()->share('currentUser', $this->currentUser);
+
+        view()->share('app_name', 'The Gospel Coalition Job Board');
+        view()->share('date_short', 'm d Y');
+
+        view()->share('today', Carbon::today()->format('Y-m-d'));
+        view()->share('beforeToday', Carbon::today()->subDays(650)->format('Y-m-d G:H:i'));
         view()->share('loggedIn', Auth::check());
         view()->share('user', Auth::user());
+
     }
 }
