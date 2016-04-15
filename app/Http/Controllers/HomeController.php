688103a49c4ae21319253f3acd4a01436928b74c
Steven Grant <steven@ghijk.co.uk>
2016-04-15 23:38:03 +0100

    allowing our variables to be used in the home controller by referencing the parent constructor

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,12 +1,19 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use Carbon\Carbon;
 use App\Job;
 use App\Http\Requests;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
     /**
      * Create a new controller instance.
      *
@@ -20,7 +27,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $jobs = Job::with('user')->orderBy('created_at', 'desc')->paginate(5);
+        $jobs = Job::with('user')->orderBy('created_at', 'desc')->paginate(10);
         return view('welcome', compact('jobs'));
     }
 }
