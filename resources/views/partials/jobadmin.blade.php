0ad12589dae86f0c6fe1f47c3e776875b1e3eeb1
Steven Grant <steven@ghijk.co.uk>
2016-05-11 17:30:40 +0100

    updated the loggedin check logic

--- resources/views/partials/jobadmin.blade.php
+++ resources/views/partials/jobadmin.blade.php
@@ -1,12 +1,4 @@
-@if($user)
-
-        @if($job->user->id == $user->id)
-            <a href="/jobs/{{ $job->id }}/edit" class="btn btn-default"><i class="fa fa-pencil"></i> Edit</a>
-
-            <button type="submit" onclick="return confirm('Are you sure?')" class="btn btn-danger">
-                <i class="fa fa-trash"></i> Delete Job
-            </button>
-
-        @endif
-
+@if($loggedIn and ($job->user_id == $currentUser->id))
+    <a href="/jobs/{{ $job->id }}/edit" class="btn btn-default"><i class="fa fa-pencil"></i> Edit</a>
+    <button type="submit" onclick="return confirm('Are you sure you wish to delete this job?')" class="btn btn-danger"><i class="fa fa-trash"></i> Delete Job</button>
 @endif
