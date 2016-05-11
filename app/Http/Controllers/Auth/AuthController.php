f8d8d64b458af981f94d5e09588e2a41b1f67445
Steven Grant <steven@ghijk.co.uk>
2016-05-11 16:30:45 +0100

    Creates a user profile when the user is registered, rather than doing it separately

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\User;
+use App\Profile;
 use Validator;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ThrottlesLogins;
@@ -65,10 +66,16 @@ class AuthController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
+
+        $user->profile()->save(new Profile);
+
+        return $user;
+        
     }
 }
