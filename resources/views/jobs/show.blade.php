4d14cc2586b0a6dcef59a6f40a0174e768ebb5cb
Steven Grant <steven@ghijk.co.uk>
2016-05-11 17:29:12 +0100

    adding some more things to show job

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -11,6 +11,7 @@
       <div class="col-md-8">
           <h1>{{ $job->title }}</h1>
           <hr>
+          <p><label class="label-success">{{ $job->job_type }}</label> <i class="fa fa-map-marker" aria-hidden="true"></i> {{ $job->job_location }}</p>
       </div>
   </div>
 @stop
