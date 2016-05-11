36b712a70132e15e2844e1730350cf9e448f7421
Steven Grant <steven@ghijk.co.uk>
2016-05-11 17:01:38 +0100

    Not relying on the cdn as we dont always have a connection locally

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,19 +7,11 @@
 
     <title>The Gospel Coalition Job Board - @yield('title')</title>
 
-    <!-- Fonts -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
-    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
-
     <!-- Styles -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="/bootstrap.css">
     {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
 
     <style>
-        body {
-            font-family: 'Lato';
-        }
-
         .fa-btn {
             margin-right: 6px;
         }
@@ -36,8 +28,8 @@
     </div>
 
     <!-- JavaScripts -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="/jquery.js"></script>
+    <script src="/bootstrap.js"></script>
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
     @yield('scripts')
 </body>
