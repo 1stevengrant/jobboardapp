607295b80b191f7325f1c040f91035b17b406870
Steven Grant <steven@ghijk.co.uk>
2016-05-12 14:04:22 +0100

    showing the profile info on the show view

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -4,40 +4,36 @@
         <div class="col-md-6 col-md-offset-3">
             <h1>{{ $user->name }}</h1>
 
-            <pre>{{ $user }}</pre>
+            <ul class="list-group">
+                @if($user->profile->org_name)
+                <li class="list-group-item">
+                    {{ $user->profile->org_name }}
+                </li>
+                @endif
 
-            {{--@if($user->profile)--}}
-            {{--<ul class="list-group">--}}
-                {{--@if($user->org_name)--}}
-                {{--<li class="list-group-item">--}}
-                    {{--{{ $user->org_name }}--}}
-                {{--</li>--}}
-                {{--@endif--}}
+                @if($user->profile->org_type)
+                <li class="list-group-item">
+                    {{ $user->profile->org_type }}
+                </li>
+                @endif
 
-                {{--@if($user->org_name)--}}
-                {{--<li class="list-group-item">--}}
-                    {{--{{ $user->org_type }}--}}
-                {{--</li>--}}
-                {{--@endif--}}
+                @if($user->profile->org_url)
+                <li class="list-group-item">
+                    <a href="{{ $user->profile->org_url }}">Visit our website</a>
+                </li>
+                @endif
 
-                {{--@if($user->org_name)--}}
-                {{--<li class="list-group-item">--}}
-                    {{--<a href="{{ $user->org_url }}">Visit our website</a>--}}
-                {{--</li>--}}
-                {{--@endif--}}
+                @if($user->profile->org_twitter)
+                <li class="list-group-item">
+                    <a href="{{ url('https://twitter.com' . $user->profile->org_twitter) }}">Find us on Twitter</a>
+                </li>
+                @endif
 
-                {{--@if($user->org_name)--}}
-                {{--<li class="list-group-item">--}}
-                    {{--<a href="{{ url('https://twitter.com' . $user->org_twitter) }}">Find us on Twitter</a>--}}
-                {{--</li>--}}
-                {{--@endif--}}
+            </ul>
 
-            {{--</ul>--}}
-            {{--@endif--}}
-
-            {{--@if($loggedIn and ($currentUser->id == $user->id))--}}
-                {{--<a href="{{ url('profile/' . $user->id . '/edit') }}" class="btn btn-default">Edit Profile</a>--}}
-            {{--@endif--}}
+            @if($loggedIn and ($currentUser->id == $user->id))
+                <a href="{{ url('profile/' . $user->id . '/edit') }}" class="btn btn-default">Edit Profile</a>
+            @endif
 
         </div>
     </div>
